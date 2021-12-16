d72687687dde5b0320eb5071dfd97a3494a91b68
ntandoyenkosi1 <s.ntando99@gmail.com>
2021-12-16 23:31:23 +0200

    Modified a few things

--- src/pages/layout/HomeSideBar.tsx
+++ src/pages/layout/HomeSideBar.tsx
@@ -4,7 +4,7 @@ const HomeSideBar=()=>{
         document.getElementById("mySidebar")!.style.display = "none";
     }
     return <div className="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left" style={{width:"200px"}} id="mySidebar">
-            <img src={icon} alt="logo" className="rounded mx-auto d-block" style={{width:"90px", height:"90px"}}/>
+            <img src="https://i.ibb.co/rQSvLPk/goat-1.png" alt="logo" className="rounded mx-auto d-block" style={{width:"90px", height:"90px"}}/>
             <button className="w3-bar-item w3-button w3-large w3-hide-large" onClick={w3_close}>Close &times;</button>
             <div style={{justifyContent:"center", alignItems:"center"}} id="sidebar"></div>
             </div>
